9879bff104fc3c00a2a9f7bc01393dadeb6607d3
Johannes Rudolph <jrudolph@meshcloud.io>
2023-03-21 12:34:19 +0100

    feat: upgrade to singer-sdk v0.22.0
    this version of the sdk has improved support for pagination
    making our implementation a little simpler

--- tap_concourse/client.py
+++ tap_concourse/client.py
@@ -1,19 +1,31 @@
 """REST client handling, including MeshObjectStream base class."""
 
-from typing import Any, Iterable, Optional, cast
-
-import requests
+import re
 import json
 from pathlib import Path
-from typing import Any, Optional, cast
+from urllib.parse import parse_qsl
 
-from singer_sdk import typing as th  # JSON schema typing helpers
 from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import BearerTokenAuthenticator
-from singer_sdk.plugin_base import PluginBase as TapBaseClass
+from singer_sdk.pagination import BaseHATEOASPaginator
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
- 
+
+from singer_sdk.pagination import BaseHATEOASPaginator
+
+class ConcoursePaginator(BaseHATEOASPaginator):
+    def get_next_url(self, response):
+        links = response.headers['link'].split(',')
+        
+        for x in links:
+            match = re.search('<(.*)>; rel="next"', x)
+            next_link = match.group(1)
+            if next_link:
+                return next_link
+
+        return None
+
+
 
 # todo: authentication via https://github.com/concourse/concourse/issues/1122
 # for now just enter a full-blown bearer token retrieved via `fly lo`
@@ -34,6 +46,18 @@ class ConcourseStream(RESTStream):
             token=self.config.get("auth").get("bearer_token")
         )
     
+    def get_new_paginator(self) -> ConcoursePaginator:
+        return ConcoursePaginator()
+
+    def get_url_params(self, context, next_page_token):
+        params = {}
+
+        # Next page token is a URL, so we can to parse it to extract the query string
+        if next_page_token:
+            params.update(parse_qsl(next_page_token.query))
+
+        return params
+
     @property
     def schema(self) -> dict:
         """Get dynamic schema including the configured tag schema
@@ -46,49 +70,4 @@ class ConcourseStream(RESTStream):
         schema = json.loads(Path(schema_filepath).read_text())
 
         return schema
-
-    def prepare_request(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> requests.PreparedRequest:
-        """Prepare a request object.
-
-        If partitioning is supported, the `context` object will contain the partition
-        definitions. Pagination information can be parsed from `next_page_token` if
-        `next_page_token` is not None.
-
-        Args:
-            context: Stream partition or context dictionary.
-            next_page_token: Token, page number or any request argument to request the
-                next page of data.
-
-        Returns:
-            Build a request with the stream's URL, path, query parameters,
-            HTTP headers and authenticator.
-        """
-        http_method = self.rest_method
-        
-        # the next_page_token is be the next page link, if set use it to overwrite the URL
-        url: str = next_page_token if next_page_token is not None else self.get_url(context)
-        
-        params: dict = self.get_url_params(context, next_page_token)
-        request_data = self.prepare_request_payload(context, next_page_token)
-        headers = self.http_headers
-
-        authenticator = self.authenticator
-        if authenticator:
-            headers.update(authenticator.auth_headers or {})
-            params.update(authenticator.auth_params or {})
-
-        request = cast(
-            requests.PreparedRequest,
-            self.requests_session.prepare_request(
-                requests.Request(
-                    method=http_method,
-                    url=url,
-                    params=params,
-                    headers=headers,
-                    json=request_data,
-                ),
-            ),
-        )
-        return request
+ 
--- tap_concourse/streams.py
+++ tap_concourse/streams.py
@@ -1,12 +1,7 @@
 """Stream type classes for tap-concourse."""
 
-import requests
-import re
-
-from typing import Any, Optional
 from pathlib import Path
 from singer_sdk import typing as th  # JSON Schema typing helpers
-from singer_sdk.plugin_base import PluginBase as TapBaseClass
 from tap_concourse.client import ConcourseStream
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
@@ -16,14 +11,4 @@ class BuildsStream(ConcourseStream):
     name = "builds"
     records_jsonpath = "$.[*]"
     primary_keys = ["id"]
-
-    def get_next_page_token(self, response: requests.Response, previous_token: Optional[Any]) -> Any:
-        links = response.headers['link'].split(',')
-        
-        for x in links:
-            match = re.search('<(.*)>; rel="next"', x)
-            next_link = match.group(1)
-            if next_link:
-                return next_link
-
-        return None
+       
